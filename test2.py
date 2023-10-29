1fffe4e280bc11f1ad4c08ec2fdd10f1e2fe8363
Blaise Skoletsky <blaiseskoletsky@gmail.com>
2023-10-29 14:28:05 -0700

    nmnm

---
+++ test2.py
@@ -0,0 +1,12 @@
+import zmq
+
+context = zmq.Context()
+
+#  Socket to talk to server
+print("Connecting to hello world server…")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5555")
+
+#  Do 10 requests, waiting each time for a response
+
+socket.send(b"Message from 361")
