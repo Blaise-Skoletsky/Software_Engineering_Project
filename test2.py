2d6c5366f4eabf26fde50daec3869d3d783a5e3a
Blaise Skoletsky <blaiseskoletsky@gmail.com>
2023-11-01 22:03:35 -0700

    gsfgfd

--- test2.py
+++ test2.py
@@ -3,7 +3,6 @@ import zmq
 context = zmq.Context()
 
 #  Socket to talk to server
-print("Connecting to hello world server…")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
 
