d030bd42b3f11a492dadf2364be69de68e899832
Blaise Skoletsky <blaiseskoletsky@gmail.com>
2023-11-03 11:53:22 -0700

    Update soccerle.js

--- soccerle.js
+++ soccerle.js
@@ -1,1 +1,20 @@
-console.log("Hleoo")
+class player {
+    constructor(name, club, country, position){
+        this.name = name
+        this.club = club
+        this.country = country
+        this.position = position
+    }
+}
+
+var mbappe = new player("Mbappe", "France", "France", "Striker")
+var clubBar = document.getElementById("player-box")
+
+clubBar.addEventListener('click', function() {
+
+    console.log("hello")
+
+
+})
+
+//club-bar
