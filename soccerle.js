c54decdc285459d987a347759aecc0f9475611a1
Blaise Skoletsky <blaiseskoletsky@gmail.com>
2023-11-04 12:51:50 -0700

    started on js

--- soccerle.js
+++ soccerle.js
@@ -1,1 +1,18 @@
-console.log("Hleoo")
+
+var inputPlayer = document.getElementById("player-box")
+
+var playerGuess = 0
+
+
+
+inputPlayer.addEventListener("keyup", function (event){
+    var boxNum = document.getElementById('guess-num')
+
+
+    if (event.keyCode == 13){
+        playerGuess = inputPlayer.value
+        inputPlayer.value=''
+        boxNum.textContent = parseInt(boxNum.textContent) - 1
+        
+    }
+})
