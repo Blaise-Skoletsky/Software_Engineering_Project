3d5e9246a9ff6f2faca4d28c888042cfde143423
Blaise Skoletsky <blaiseskoletsky@gmail.com>
2023-11-04 23:53:30 -0700

    fdfd

--- soccerle.js
+++ soccerle.js
@@ -1,8 +1,10 @@
 
 var inputPlayer = document.getElementById("player-box")
+var inputClub = document.getElementById("club-bar")
+var inputPosition = document.getElementById("position-bar")
+var inputCountry = document.getElementById("country-bar")
 
 var playerGuess = 0
-
 
 
 inputPlayer.addEventListener("keyup", function (event){
@@ -14,5 +16,19 @@ inputPlayer.addEventListener("keyup", function (event){
         inputPlayer.value=''
         boxNum.textContent = parseInt(boxNum.textContent) - 1
         
+    
     }
 })
+
+
+inputClub.addEventListener("keyup", function(event){
+    if(event.keyCode == 13){
+        var  countryBox3 = document.getElementById("clubbox3")
+
+
+    }
+
+
+
+})
+
