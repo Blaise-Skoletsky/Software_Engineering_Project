534a97282db8429685bde52fecec6d6cf01a0aad
Blaise Skoletsky <blaiseskoletsky@gmail.com>
2023-11-05 13:50:46 -0800

    bbcvb

--- soccerle.js
+++ soccerle.js
@@ -25,7 +25,14 @@ inputClub.addEventListener("keyup", function(event){
     if(event.keyCode != 13){
         return
     }  
-    var  countryBox3 = document.getElementById("clubbox3")
+    var  clubBox3 = document.getElementById("clubbox3")
+    console.log(clubBox3.style['background-color'])
+    if (clubBox3.style.backgroundColor === 'green' || clubBox3.style.backgroundColor === 'red' ||clubBox3.style.backgroundColor === 'yellow'){
+        inputClub.value = ''
+        console.log('box is colored')
+        return
+    }
+
         // if the third box is either green, yellow or red already, don't do anything
         // Else, run code.
 
