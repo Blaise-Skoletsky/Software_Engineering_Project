48d8d7dcde9ed981b0764d065f37ee471954a742
Blaise Skoletsky <blaiseskoletsky@gmail.com>
2023-11-30 16:45:20 -0800

    removed some tags, finished things up looking good!

--- server.js
+++ server.js
@@ -15,14 +15,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/socket.io-client', express.static(path.join(__dirname, 'node_modules/socket.io-client/dist')));
 app.use(bodyParser.json())
 
-// Serve your HTML file when accessing the root URL
+// Serves HTML file. 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/public/page.html'));
-});
-
-// Serve client.js file
-app.get('/socccerle.js', function (req, res) {
-    res.sendFile(path.join(__dirname, '/public/soccerle.js'));
 });
 
 
@@ -46,11 +41,10 @@ io.on('connection', socket => {
 
 app.post('/', function(req, res){
     playerInfo = req.body.playerInfo
-    //console.log(playerInfo)
     res.status(200).send('success')
 })
 
-// Move this line to the end of the code
+
 server.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
 });
